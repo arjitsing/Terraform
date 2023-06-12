9f6f9b41292cd0551a652c0d1c3a8f8091e2b257
arjitsing <arjitsingh13@gmail.com>
2023-06-12 18:06:06 +0530

    first commit from section_5

---
+++ provisioner_1.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "AKIAXXNOCQ2ZX6OBMMN7"
+  secret_key = "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"
+}
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.0.20230517.1-kernel-6.1-x86_64"]
+  }
+}
+resource "aws_instance" "example" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  key_name = "first_keypair"
+  
+  
+  provisioner "remote-exec" {
+    connection {
+    type ="ssh"
+    user = "ec2-user"
+    private_key =file("../section_5/first_keypair.pem")
+    host= self.public_ip
+
+    }
+    #on_failure=continue
+    inline = [
+        "sudo yum install -y httpd",
+        "sudo systemctl start httpd"
+    ]
+  }
+
+}
+
+output "ami_id" {
+  value=aws_instance.example.ami
+}
